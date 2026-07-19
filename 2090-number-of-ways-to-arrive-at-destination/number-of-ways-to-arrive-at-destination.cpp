5e2342283a05912a2cd7c30c5866ac2cfbf6eb5f
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-07-20 00:49:47 +0530

    Time: 7 ms (66.56%) | Memory: 35.7 MB (57.14%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,13 +1,13 @@
-#define p pair< long long , int> 
+using pp = tuple<long long  , int > ;
 class Solution {
-
-    int const MOD = 1e9 + 7;
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        // int n = roads.size();
-        vector<vector<pair< int , int>>> adj(n);
 
-        for(auto r : roads){
+        const int MOD = 1e9 + 7 ;
+
+        vector<vector<pair<int , int>>> adj( n );
+
+        for( auto & r : roads){
             int u = r[0];
             int v = r[1];
             int w = r[2];
@@ -15,37 +15,43 @@ public:
             adj[u].push_back({v , w});
             adj[v].push_back({u , w});
         }
+        
+        vector<pair<int , long long >> dist(n , { 0 , LLONG_MAX});
 
-        priority_queue< p , vector<p> , greater<p>> pq;
 
-        pq.push({0 , 0 });
+        priority_queue<pp , vector<pp> , greater<pp>> pq;
 
-        vector<pair< long long , int>> node( n , {LLONG_MAX , 0});
 
-        node[0].first = 0;
-        node[0].second = 1;
 
-        while(!pq.empty()){
-            auto [x , y] = pq.top();
+
+        pq.push({ 0 , 0  });
+        dist[0].second = 0 ;
+        dist[0].first = 1;
+
+        while( !pq.empty()){
+            auto [ d , node  ] = pq.top();
+
             pq.pop();
+             if (d > dist[node].second)
+        continue;
 
-            for( auto [a , w] : adj[y]){
-                
-                if((long long)w + x < node[a].first ){
-                    node[a].first = w+x;
-                    node[a].second = node[y].second;
-                    pq.push({w+x , a});
-                }else if( w+x == node[a].first){
-                    node[a].second = (node[y].second + node[a].second) % MOD;
+            for( auto & [a , w ] : adj[node]){
+
+                if( (long long )w + d < dist[a].second){
+                    dist[a].second = w + d;
+                    dist[a].first = dist[node].first;
+                    pq.push({dist[a].second, a});
+                }else if((long long ) w + d == dist[a].second){
+                    dist[a].first = (dist[a].first +dist[node].first) %MOD;
+                    // dist[a].first %= MOD;
+                    // pq.push({ w + d , a });
                 }
-
-                
             }
-
         }
 
-        return node[n-1].second;
+        // cout << dist[n-1].second;
 
+        return dist[n-1].first;
 
     }
 };
