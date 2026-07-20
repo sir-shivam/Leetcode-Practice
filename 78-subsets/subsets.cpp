16db29868e212a6dd8d6413c6aa088f212dfd2fc
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-07-21 03:35:14 +0530

    Time: 3 ms (27.83%) | Memory: 10 MB (60.38%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,39 +1,24 @@
 class Solution {
-
-    vector<vector<int>> ans;
-
-    void help(int i ,  vector<int> &  curr , vector<int> & nums ){
-
-        if( i == nums.size()) return ;
-        // take this i
-
-
-        curr.push_back( nums[i]);
-        ans.push_back(curr);
-
-        help( i + 1 , curr , nums ); 
-
-
-        // remove this i and move forword 
-
-        curr.pop_back();
-        // ans.push_back(curr);
-
-        help( i+1 , curr , nums);
-
-    }
-
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         
-        vector<int> curr = {};
-        ans.push_back({});
-        help( 0 , curr , nums );
+        int n = nums.size();
 
+        vector<vector<int>> ans;
 
+        for( int x = 0 ; x < ( 1 << n ) ; x++){
+
+            vector<int> curr;
+
+            for( int i = 0 ; i < n ; i++){
+                if( x & ( 1 << i )){
+                    curr.push_back(nums[i]);
+                }
+
+            }
+                ans.push_back(curr);
+        }
 
         return ans;
-
-
     }
 };
