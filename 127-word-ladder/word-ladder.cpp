613c7d534d3dcb76ab8cfb007422afceef3eceb0
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-07-28 22:55:08 +0530

    Time: 2387 ms (5.00%) | Memory: 51.2 MB (5.01%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,56 @@
+using pp = pair<int ,  string >;
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        map<string ,int > mpp;
+
+        for( auto & a : wordList){
+            mpp[a] = 1e9 ;
+        }
+        
+
+        mpp[beginWord] = 1;
+
+        if(!mpp.count(endWord)) return 0 ;
+
+        priority_queue<pp , vector<pp> , greater<pp>> pq;
+
+        int m = wordList.size();
+
+        vector<int> dis( m , 0 );
+
+        pq.push({ 1, beginWord});
+
+        while( !pq.empty()){
+            
+            auto [ d , word] = pq.top();
+            pq.pop();
+
+            if( word == endWord) return d;
+
+            if( d <=  mpp[word]){
+                mpp[word] = d ;
+
+                for(int i = 0 ; i < word.size() ; i++){
+                char s = word[i];
+                for(char a = 'a' ; a <= 'z' ; a++){
+                    word[i] = a;
+                    if(mpp.count(word)){
+                        pq.push({ d+1 , word});
+                    }
+                    word[i] = s;
+                }
+            }
+
+            }
+
+            
+        }
+
+        return 0;
+
+
+    }
+};
