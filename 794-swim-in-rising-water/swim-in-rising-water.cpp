8e5e13d4444762f4e196ca18b6400c323a4df3d2
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-07-30 10:52:45 +0530

    Time: 3 ms (87.54%) | Memory: 13.5 MB (54.94%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,47 +1,94 @@
-using pp = tuple< int , int , int>;
+using pp = tuple<int, int, int>;
+
+class DisJointset {
+
+public:
+    vector<int> sz;
+    vector<int> par;
+
+    DisJointset(int n) {
+        sz.resize(n + 1, 1);
+
+        par.resize(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            par[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (par[x] == x)
+            return x;
+
+        return par[x] = find(par[x]);
+    }
+
+    void connect(int x, int y) {
+
+        int u = find(x);
+        int v = find(y);
+
+        if (u == v)
+            return;
+
+        if (sz[u] >= sz[v]) {
+            par[v] = u;
+            sz[u] += sz[v];
+        } else {
+            par[u] = v;
+            sz[v] += sz[u];
+        }
+    }
+};
 
 class Solution {
 
-    vector<pair<int , int>> dir = { { 0 ,1 } , { 1 , 0 } , { -1 , 0 } , { 0 , -1}};
+    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
 public:
     int swimInWater(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
-        
-        vector<vector<pair<int , int > >> dis( n , vector<pair<int , int>> (m , {0  , 1e9}));
 
+        vector<tuple<int, int>> dis;
 
-        int i = 0 ;
-        int j = 0 ;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int num = i * n + j;
+                dis.push_back({grid[i][j], num});
+            }
+        }
 
-        priority_queue<pp , vector<pp> , greater<pp>> pq;
+        sort(dis.begin(), dis.end());
 
-        pq.push({grid[0][0], 0, 0});
+        DisJointset dsu(n * m + 1);
+        vector<vector<bool>> active(n, vector<bool>(n, false));
 
-        while( !pq.empty() ){
-            auto [d , i , j ] = pq.top();
+        for (auto& [height, cell] : dis) {
 
-            pq.pop();
+            int r = cell / n;
+            int c = cell % n;
 
+            active[r][c] = true;
 
-            if( d < dis[i][j].second){
-                dis[i][j].second = d ;
+            for (auto& [dx, dy] : dir) {
 
-                for(auto & [x , y ] : dir ){
-                int ix = i + x;
-                int jx = j + y;
+                int nr = r + dx;
+                int nc = c + dy;
 
-                if( ix >= 0 && jx >= 0 && ix < n && jx < m ){
-                    pq.push({ max(grid[ix][jx] , dis[i][j].second) , ix , jx});
-                    
-                    }
+                if (nr < 0 || nc < 0 || nr >= n || nc >= n)
+                    continue;
+
+                if (active[nr][nc]) {
+                    dsu.connect(cell, nr * n + nc);
                 }
-
             }
 
+            if (dsu.find(0) == dsu.find(n * n - 1))
+                return height;
         }
-        return dis[n-1][m-1].second ;
 
+        return -1;
     }
 };
