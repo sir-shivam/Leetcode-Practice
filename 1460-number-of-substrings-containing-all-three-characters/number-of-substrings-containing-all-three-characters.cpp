95cbc1479dd7b1d9394e75ab2e628c4c032b6170
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-06 11:44:58 +0530

    Time: 6 ms (68.22%) | Memory: 11.2 MB (28.42%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,21 +5,19 @@ public:
 
         int n = s.size();
 
-        map< char , int> mpp;
+        // map< char , int> mpp;
+        vector<int> mpp( 3 , 0 );
 
         int ans = 0 ;
 
         int j = 0 ;
 
         for( int i = 0 ; i < n ; i++){
-            mpp[s[i]] ++;
+            mpp[s[i] - 'a'] ++;
 
-            while( j <= i && mpp.size() == 3 ){
+            while( j <= i && ( mpp[0] > 0 && mpp[1] > 0 && mpp[2] >  0 ) ){
                 ans += n - i ;
-                mpp[s[j]] --;
-                if( mpp[s[j]] == 0 ){
-                    mpp.erase(s[j]);
-                }
+                mpp[s[j] -'a'] --;
                 j++;
             }
         }
