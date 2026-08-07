fbdf60d9b664a29c2826825806d158865783e4a0
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-07 10:28:27 +0530

    Time: 40 ms (36.11%) | Memory: 299.8 MB (61.11%) - LeetSync

---
+++ 4114-incremental-even-weighted-cycle-queries/incremental-even-weighted-cycle-queries.cpp
@@ -0,0 +1,85 @@
+class DisJointSet{
+
+    public:
+
+    vector<int> sz , par , sum;
+    int n ;
+
+    DisJointSet(int n ){
+        this -> n = n ;
+
+        sz.resize(n+1 , 1 );
+
+        par.resize(n+1);
+
+        sum.resize(n+1 , 0 );
+
+        for( int i = 0 ; i <= n ; i++){
+            par[i] = i ;
+        }
+    }
+
+    int find( int x){
+        if( par[x] == x ) return x;
+
+        int p = par[x];
+        par[x] = find(par[x]);
+        sum[x] ^= sum[p];
+        return par[x];
+    }
+
+    bool add(int a , int b , int w ){
+        find(a);
+        find( b);
+        int t = ( sum[a] ^ sum[b]);
+        return (t ^ w ) == 0 ;
+    }
+
+    bool unite(int a , int b  , int w ){
+        int u = find(a);
+        int v = find(b);
+
+        if( u == v ) return true;
+
+        int x = sum[a] ^ sum[b] ^ w ;
+
+        if( sz[u] >= sz[v]){
+
+            sum[v] = x;
+            par[v] = u ;
+            sz[u] += sz[v];
+        }else{
+        
+            sum[u] = x;
+            par[u] = v ;
+            sz[v] += sz[u];
+        }
+
+        return false;
+    }
+};
+
+class Solution {
+public:
+    int numberOfEdgesAdded(int n, vector<vector<int>>& edges) {
+        
+        DisJointSet dsu( n +1 );
+
+        int ans = 0 ;
+        for(auto & ed: edges){
+            int u = ed[0];
+            int v = ed[1];
+            int w = ed[2];
+
+            if(dsu.unite( u , v  , w)){
+                if(dsu.add(u , v  , w)){
+                    ans ++;
+                }
+            }else{
+                ans ++;
+            }
+        }
+
+        return ans;
+    }
+};
