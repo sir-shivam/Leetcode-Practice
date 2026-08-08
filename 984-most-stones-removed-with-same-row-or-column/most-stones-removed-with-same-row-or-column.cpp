2915317b1cdcd961a3dfbd57977d31c5a5b5d879
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-08 11:31:56 +0530

    Time: 7 ms (91.22%) | Memory: 22.9 MB (41.97%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,43 +1,42 @@
-class DisJointset{
-
+class DisJointSet{
     public:
 
-    vector<int> sz;
-    vector<int> par;
+    int n ;
+    vector<int> par , sz ;
 
-    DisJointset(int n ){
-        sz.resize(n+ 1 , 1  );
+    DisJointSet(int n ){
+        this -> n = n ;
 
-        par.resize(n+1 );
+        par.resize( n +1 );
 
-        for( int i = 0 ; i < n ; i++){
-            par[i] =  i ;
+        sz.resize(n+1 , 1);
+
+        for( int i = 0 ; i < n ;i ++){
+            par[i] = i ;
         }
     }
 
 
     int find( int x ){
-        if(par[x] == x) return x ;
-
-        return par[x] = find( par[x]); 
+        if( par[x] == x ) return x ;
+        return par[x] = find(par[x]);
     }
 
-    void connect( int x , int y  ){
-        
+    void unite(int x , int y ){
         int u = find( x );
-        int v = find( y );
+        int v = find( y);
 
         if( u == v ) return ;
 
-        if( sz[u] >= sz[v]){
+        if(sz[u] >= sz[v]){
             par[v] = u ;
-            sz[u]+= sz[v];
+            sz[u] += sz[v];
         }else{
-            par[v] = u ;
+            par[u] = v ;
             sz[v] += sz[u];
         }
-
     }
+
 };
 
 class Solution {
@@ -47,38 +46,31 @@ public:
         int n = 0 ;
         int m = 0 ;
 
-        for( auto & st : stones){
-            n = max( n , st[0]);
-            m = max( m , st[1]);
+        for(auto & st : stones){
+            n = max( n, st[0] +1 );
+            m = max(m , st[1] +1 );
         }
 
-        DisJointset dsu(n + m + 2 );
-        map<int , int> mpp;
-
+        DisJointSet dsu( n +  m + 20 );
+         unordered_set<int> used;
 
         for( auto & st : stones){
-            int u = st[0];
-            int v = st[1];
+            int row = st[0];
+            int col = st[1] + n  ;
 
-            int row = u ;
-            int col = v + n + 1;
-
-            dsu.connect( row , col);
-
-            mpp[row] = 1;
-            mpp[col] = 1;
+            dsu.unite(row , col);
+            used.insert(row);
+            used.insert(col);
         }
 
         int cnt = 0 ;
-        for(auto & [a , b ] : mpp){
-            if(dsu.find(a) == a){
-                cnt ++;
-            }
+
+
+        for(auto & a : used){
+            if(dsu.find(a) == a) cnt++;
         }
 
-        int st = stones.size();
-
-        return st - cnt ;
+        return stones.size() - cnt ;
 
 
 
