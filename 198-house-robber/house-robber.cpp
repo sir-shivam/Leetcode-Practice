b73369f817fd0522d1d056261a13a6e19a0e81fe
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-10 07:40:37 +0530

    Time: 3 ms (6.07%) | Memory: 10.7 MB (24.32%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,40 +1,21 @@
 class Solution {
-    int help(int i, vector<int>& nums, int prev, vector<vector<int>>& dp) {
-
-        int n = nums.size();
-        if (i >= n)
-            return 0;
-        if (dp[i][prev] != -1)
-            return dp[i][prev];
-        // dont rob
-        int dont = help(i + 1, nums, prev, dp);
-
-        // rob
-        int rob = 0;
-        if (prev + 1 != i) {
-            rob = nums[i] + help(i + 1, nums, i, dp);
-        }
-
-        return dp[i][prev] = max(dont, rob);
-    }
-
 public:
     int rob(vector<int>& nums) {
+
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        if( n== 1 ) return nums[0];
 
-        for (int i = n-1; i >= 0 ; i--) {
-            for (int prev = n - 1; prev >= 0; prev--) {
-                int dont = dp[i + 1][prev]; //  help(i+1 , nums , prev , dp);
-                int rob = 0;
-                if ( prev == n || prev + 1 != i) {
-                    rob = nums[i] + dp[i+1][i] ;// help(i + 1, nums, i, dp);
-                }
+        vector<int> dp( n , 0 );
 
-            dp[i][prev] = max(dont , rob);
-            }
+        dp[0] = nums[0] ;
+        dp[1] = max( dp[0] , nums[1]);
+        
+
+        for( int i = 2 ; i < n ; i++){
+
+            dp[i] = max( dp[i-1] , nums[i] + dp[i-2]);
         }
 
-        return dp[0][n-1] ;// help(0, nums, n - 1, dp);
+        return dp[n-1];
     }
 };
