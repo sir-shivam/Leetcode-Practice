c28634000e08c0ce695a76a1c987913c5db4da71
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-10 06:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -23,8 +23,22 @@ class Solution {
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp( n+1 , -1);
+        vector<int> dp( n+1 , 0);
+        dp[0] = 1;
+
+        for( int i = 1 ; i <=  n ; i++){
+
+            int one = dp[i-1];
+
+            int two = 0 ; 
+            if( i >= 2 ){
+                two = dp[i-2];
+            }
+            dp[i] = one + two;
+        }
+
+        return dp[n];
         
-        return help( n  , dp );
+        // return help( n  , dp );
     }
 };
