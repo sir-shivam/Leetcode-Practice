d855835d8aa0b151433b6c271119a7d1265bb540
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-10 07:28:42 +0530

    Time: 103 ms (48.27%) | Memory: 241.7 MB (24.42%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,17 +1,15 @@
 class Solution {
 
     map<int , int> mpp;
-
-    map<int , map<int , int>> dp;
     int n ;
 
-    bool help( int i , long long  k ,  vector<int>& stones ){
+    bool help( int i , int k ,  vector<int>& stones ,  vector<vector<int>>  &dp ){
 
         // k-1 steps 
 
         if( i == n-1 ) return 1;
-        if(dp.count(i) && dp[i].count(k) ) return dp[i][k];
-        for( long long m = -1 ; m <= 1 ; m++){
+        if( dp[i][k] != -1) return dp[i][k];
+        for( int m = -1 ; m <= 1 ; m++){
             if( k + m  == 0 ) continue;
             if(mpp.count(stones[i]+ k+m)){
 
@@ -19,7 +17,7 @@ class Solution {
                     return dp[i][k] =  true;
                 }
 
-                if(help( mpp[stones[i]+k+m] , k+m , stones )){
+                if(help( mpp[stones[i]+k+m] , k+m , stones , dp)){
                     return dp[i][k] =  true;
                 };
             }
@@ -35,12 +33,12 @@ public:
         n = stones.size();
         if( stones[1] != 1) return false;
 
-        // vector<vector<int>> dp( n , vector<int> ( stones[n-1] , -1));
+        vector<vector<int>> dp( n , vector<int> ( n +1 , -1));
 
         for( int i = 0 ;i < n ; i++){
             mpp[stones[i]] = i ;
         }
 
-        return help( 1 , 1 , stones );
+        return help( 1 , 1 , stones , dp);
     }
 };
