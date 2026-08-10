e97cc0232429d1099d416acf5ae18ecf56da5f8e
Gupta Shivam <143452638+sir-shivam@users.noreply.github.com>
2026-08-10 06:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,37 +1,30 @@
 class Solution {
-    int help(int n , vector<int> &dp){
+
+    int help( int n , vector<int> & dp ){
+
+        // one step down 
+
+
+        if( n < 0  ) return 0 ;
+        if( n == 0 ) return 1 ;
+
+        if( dp[n] != -1) return dp[n];
+        int one = help( n -1 , dp );
+
+        // two step down 
+
+        int two = help( n - 2 , dp);
         
-        if(n <= 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        int one = help( n -1 , dp);
 
-        int two = 0;
-        if(n >= 2){
-            two = help(n -2 , dp);
-        }
+        return dp[n] =  one + two ;
 
-        return dp[n] =  one + two;
+
+
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1 , 0 );
-
-        dp[0]= 1;
-        dp[1] = 1;
-
-        for( int i = 2 ; i <= n ; i++){
-            int one =  dp[i-1] ;//help( n -1 , dp);
-            int two = 0;
-            if( i >= 2){
-            two = dp[i-2];// help(i  -2 , dp);
-            }
-
-            dp[i] = one + two;
-        }
-
-
-
-        return dp[n];// help(n , dp);
-
+        vector<int> dp( n+1 , -1);
+        
+        return help( n  , dp );
     }
 };
